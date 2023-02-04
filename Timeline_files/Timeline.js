19c3357182a2a3df17b8b92e1696ab7cf897e78f
theaim21 <96447424+AimanMario@users.noreply.github.com>
2023-02-03 20:49:14 -0500

    Add files via upload

---
+++ Timeline_files/Timeline.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20221227
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,352),url:'Timeline_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Timeline_files/stroke_1.png'},{rect:new IWRect(2,-2,629,4),url:'Timeline_files/stroke_2.png'},{rect:new IWRect(631,-2,4,4),url:'Timeline_files/stroke_3.png'},{rect:new IWRect(631,2,4,352),url:'Timeline_files/stroke_4.png'},{rect:new IWRect(631,354,4,5),url:'Timeline_files/stroke_5.png'},{rect:new IWRect(2,354,629,5),url:'Timeline_files/stroke_6.png'},{rect:new IWRect(-2,354,4,5),url:'Timeline_files/stroke_7.png'}],new IWSize(633,356))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Timeline_files/TimelineMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
